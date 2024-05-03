0c4259aa2ccd26f58b470d65c7331f104dce88ae
ambivalent-axiom <artmelnis@gmail.com>
2024-05-03 23:36:04 +0300

    added correct method for loops exercise8, wrong uncommented

--- basics/loops/exercise8.php
+++ basics/loops/exercise8.php
@@ -13,17 +13,29 @@
             $min = self::prompt('Min?: ');
             $max = self::prompt('Max?: ');
 
-            for ($i = $min; $i <= $max; $i++) {
-                $offset = $max - $i;
-                for ($j = $min; $j <= $offset + 1; $j++) {
-                    $a = $j + $i - 1;
-                    echo "$a   ";
-                }
-                for ($j = $min; $j <= $i - 1; $j++) {
-                    echo "$j   ";
+            for ($row = 1; $row <= $max - $min; $row++) {
+                for ($col = $min; $col <= $max; $col++) {
+                    $value = $col + $row - 1;
+                    if ($value > $max) {
+                        $value = $value - $max + $min - 1;
+                    }
+                    echo "$value ";
                 }
                 echo "\n";
             }
+
+
+//            for ($i = $min; $i <= $max; $i++) {
+//                $offset = $max - $i;
+//                for ($j = $min; $j <= $offset + 1; $j++) {
+//                    $a = $j + $i - 1;
+//                    echo "$a   ";
+//                }
+//                for ($j = $min; $j <= $i - 1; $j++) {
+//                    echo "$j   ";
+//                }
+//                echo "\n";
+//            }
         }
     }
     NumberSquare::matrix();
